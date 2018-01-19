c7dea5193f9c80c12bc4c8d52a624208f3f6a4ac
prateekkaushal7886 <prateekkaushal7886@gmail.com>
2018-01-19 15:50:12 +0530

    gallery changes

--- database/migrations/2018_01_08_170455_create_comments_table.php
+++ database/migrations/2018_01_08_170455_create_comments_table.php
@@ -15,6 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
+            $table->text('pic_id');
+            $table->text('user_id');
+             $table->text('comments');
             $table->timestamps();
         });
     }
