bf491fc377d3348c34e0212c170e31330c4053c3
prateekkaushal7886 <prateekkaushal7886@gmail.com>
2018-01-20 16:29:38 +0530

    adding comments via ajax

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -15,5 +15,53 @@ class CommentController extends Controller
     		'user_id' => Auth::user()->id,
     	]);
 
+$mydata = Comment::where('pic_id',request('pic_id'))->get();
+
+ $content= '<table class="table-striped col l12 s12 m12 table-scrollable">
+<tbody>  '; 
+
+ foreach($mydata as $view)   
+   {   
+    $content=$content. '    
+ <tr class="row"><td style = "word-wrap: break-word;padding:20px; " class="col l9"> <b>'.$view['pic_id'].' said:</b><br>
+                      '.$view['comments'].'
+                  </td>'
+         ;
+
+
+}
+$content=$content.' </tbody>
+        </table>';
+  return response($content, 200);  	
+        
+
+    }
+
+
+
+    public function new()
+    {
+    	
+
+$mydata = Comment::where('pic_id',request('pic_id'))->get();
+
+ $content= '<table class="table-striped col l12 s12 m12 table-scrollable">
+<tbody>  '; 
+
+ foreach($mydata as $view)   
+   {   
+    $content=$content. '    
+ <tr class="row"><td style = "word-wrap: break-word;padding:20px; " class="col l9"> <b>'.$view['pic_id'].' said:</b><br>
+                      '.$view['comments'].'
+                  </td>'
+         ;
+
+
+}
+$content=$content.' </tbody>
+        </table>';
+  return response($content, 200);  	
+        
+
     }
 }
